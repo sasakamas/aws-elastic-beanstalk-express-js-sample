9fca8a5997f0104596521e9f1c6b257f7d63ee6b
taro_tohoku <super_vitamin_a@yahoo.co.jp>
2022-04-26 23:59:29 +0900

    change message 2kaime

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('Test Hello World! HogeHoge'));
+app.get('/', (req, res) => res.send('Test Hello World! HogeHoge2'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
